dd3b516714d83a059bdec7e9d78d0246c1338b86
itc-n23030 <n23030@std.it-college.ac.jp>
2024-05-14 07:35:45 +0000

    collatz

--- TextBook/CH03/collatz.py
+++ TextBook/CH03/collatz.py
@@ -1,7 +1,6 @@
-collatz = lambda x : x//2 if x%2 == 0 else (x*3)+1
-
-n=int(input())
-
-while n!=1 :
-    print(collatz(n))
-    n = collatz(n)
+def collatz(x):
+    return x//2 if x%2 == 0 else (x*3)+1
+    x=int(input())
+    while x!=1:
+        print(collatz(x))
+        x=collatz(x)
