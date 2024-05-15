1dd2f94f8af61ce7ecdc52683711a28e77e66a9c
itc-n23030 <n23030@std.it-college.ac.jp>
2024-05-15 00:47:40 +0000

    fix

--- TextBook/CH03/collatz.py
+++ TextBook/CH03/collatz.py
@@ -1,6 +1,6 @@
 def collatz(x):
     return x//2 if x%2 == 0 else (x*3)+1
-    x=int(input())
-    while x!=1:
-        print(collatz(x))
-        x=collatz(x)
+
+x=int(input())
+while x!=1:
+    print(x := collatz(x))
